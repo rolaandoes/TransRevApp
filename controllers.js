fc25137540cda78d358f96a49a24d8d32fe9be0c
Rolando <rolando.jr.rodriguez@gmail.com>
2015-08-29 17:08:36 -0700

    added new/save to controller, not yet implemented

--- controllers.js
+++ controllers.js
@@ -1,5 +1,21 @@
 angular.module('reviewApp.controllers', [])
 .controller('MainCtrl', ['$scope', function($scope){
 	$scope.heading = "Airf-with a sexy side, Uber, LYft, and Public Transports";
-	$scope.reviews = 'This is how I feel about Uber'
+	$scope.reviews = 'This is how I feel about Uber';
+	$scope.saveReview = function(Review){
+		if (!localStorage.reviews){
+			localStorage.reviews = JSON.stringify([]);
+		}
+		//get existing from localStorage
+		var allReviews = JSON.parse(localStorage.reviews);
+		allReviews.push(review);
+		localStorage.reviews = JSON.stringify(allReviews)
+	};
 }])	
+.controller('ReviewsCtrl', ['$scope', function($scope){
+	if (!localStorage.reviews){
+		$scope.posts = [];
+	} else {
+		$scope.posts = JSON.parse(localStorage.reviews);
+	}
+}]);
