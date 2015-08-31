0f1606b713a249e734d45a7b4412c0c4e41e4b18
Rolando <rolando.jr.rodriguez@gmail.com>
2015-08-31 12:07:01 -0700

    have a headless questions app, now need to add author/answers

--- controllers.js
+++ controllers.js
@@ -1,6 +1,6 @@
 angular.module('reviewApp.controllers', [])
 .controller('MainCtrl', ['$scope', function($scope){
-	$scope.heading = "Airf-with a sexy side, Uber, LYft, and Public Transports";
+	$scope.heading = "Uber, LYft, and Public Transports";
 	$scope.reviews = 'This is how I feel about Uber';
 	$scope.saveReview = function(Review){
 		if (!localStorage.reviews){
